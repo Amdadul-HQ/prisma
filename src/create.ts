c2f499ea90662cf16ad7077ae7e5fdc00d401659
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-06 20:42:40 +0600

    add: get or finde post query method

---
+++ src/create.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    const result = await prisma.post.create({
+        data:{
+            title:"This is title",
+            authorName:"Amdadul",
+            content:"this is content for this post"
+        }
+    })
+
+}
+
+main()
---
+++ src/find.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+     // find all post with condition
+     const posts = await prisma.post.findMany({
+      
+    })
+    console.log(posts);
+
+    // find all post with condition
+    const post = await prisma.post.findMany({
+        where:{
+            published:false
+        }
+    })
+    console.log({post});
+
+    // find fast is like javascript find method it will returen the fast element
+    const findFast = await prisma.post.findFirstOrThrow({
+        where:{
+            published:true
+        }
+    })
+
+    console.log({findFast});
+
+    // fine Unique is like it will only find by Unique property like id slug 
+    const findUnique = await prisma.post.findUniqueOrThrow({
+        where:{
+            id:3
+        }
+    })
+
+    console.log({findUnique});
+}
+
+main()
