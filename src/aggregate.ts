e8b334220b87bbd29ac837e85146fe60447ed35d
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-07 19:13:00 +0600

    min and max method

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -4,13 +4,57 @@ const prisma = new PrismaClient();
 
 
 const aggragates = async () => {
-    const avgAge = await prisma.user.aggregate({
-        _avg:{
-            age:true
-        }
-    })
+    // finding avg age
+    // const avgAge = await prisma.user.aggregate({
+    //     _avg:{
+    //         age:true
+    //     }
+    // })
 
-    console.log(avgAge);
+    // console.log(avgAge);
+    
+    // finding sum of age
+    // const sumAge = await prisma.user.aggregate({
+    //     _sum:{
+    //         age:true
+    //     }
+    // })
+
+    // console.log(sumAge);
+
+    // finding count 
+    // const countAge = await prisma.user.aggregate({
+    //     _count:{
+    //         age:true
+    //     }
+    // })
+
+    // console.log(countAge);
+
+    // find number of record
+    // const count = await prisma.user.count()
+
+    // console.log(count);
+
+
+    // find max age
+    // const maxAge = await prisma.user.aggregate({
+    //     _max:{
+    //         age:true
+    //     }
+    // })
+
+    // console.log(maxAge)
+    
+    // find min age
+    // const minAge = await prisma.user.aggregate({
+    //     _min:{
+    //         age:true
+    //     }
+    // });
+
+    // console.log(minAge);
+    
 }
 
 aggragates()
