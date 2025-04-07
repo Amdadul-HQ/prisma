915ded702b5c63d10295600c61143fc115453717
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-04-07 16:20:11 +0600

    add pluent api and include api

--- src/relational-queries.ts
+++ src/relational-queries.ts
@@ -3,11 +3,22 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const relationalQueries = async () => {
+    // pluent api
+    // const result = await prisma.user.findUnique({
+    //     where:{
+    //         id:1
+    //     }
+    // }).post()
+
+    // include method
     const result = await prisma.user.findUnique({
         where:{
             id:1
+        },
+        include:{
+            post:true
         }
-    }).post()
+    })
 
     console.log(result);
 }
