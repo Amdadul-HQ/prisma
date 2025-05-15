3c3a4b6a3b00e21ffd8e0a6bd1e8bab5b6d87100
Amdadul Haque Bhuiyan <153616931+Amdadul-HQ@users.noreply.github.com>
2025-05-15 19:29:48 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-    // const result = await prisma.post.create({
-    //     data:{
-    //         title:"This is title",
-    //         authorName:"Amdadul",
-    //         content:"this is content for this post"
-    //     }
-    // })
+
     const post = await prisma.post.findMany()
 
     console.log(post);
